e88d888f096e61070db5b115efd099f25e1da21c
sultana-mnw <36527697+sultana-mnw@users.noreply.github.com>
2018-03-08 15:22:03 +0530

    Add files via upload

---
+++ token.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.0;
+contract token{
+    mapping(address=>uint)public balance;
+    address owner;
+    string public name;
+    string public symbol;
+    uint public decimal;
+    uint public totalsupply;
+    function token(){
+         totalsupply=100;
+         name="mnw";
+     symbol="mnw";
+     balance[msg.sender]=totalsupply;
+       decimal=0;
+     }
+    
+    modifier onlyowner(){
+        
+        owner=msg.sender;
+        _;
+    }
+    
+    function transfer(address to, uint amount)onlyowner public returns(bool){
+    
+    balance[to]=balance[to]+amount;
+    balance[msg.sender]=balance[msg.sender]-amount;
+    return true;
+     }
+    
+    function mint(uint amount)onlyowner public returns (bool){
+        require(amount>0);
+        require(totalsupply > amount);
+        
+        balance[msg.sender]=+amount;
+        return true;
+    }   
+     
+     function tranferfrom(address from,address to,uint amount)public returns(bool){
+         require(amount > 0 && balance[from]>=amount );
+         balance[to]=balance[to]+amount;
+         balance[from]=balance[from]-amount;
+         return true;
+         
+     }    
+
+        
+    
+}
